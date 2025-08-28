d73d14a8a3e1411aa0a0ce681c06c827ecd31bee
ahsanhabibakik <syedmirhabib@gmail.com>
2025-08-28 13:59:17 +0600

    chore(ui): update project files
    Deleted files:

--- src/syedhabib/src/types/activity.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectId } from 'mongodb';
-
-export interface Activity {
-  _id?: ObjectId;
-  action: string; // e.g., "Created post"
-  timestamp: string; // ISO
-  meta?: Record<string, unknown>;
-}
