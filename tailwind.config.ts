01c70390adecca31cb250a246a2e1b69aa56ca4e
ahsanhabibakik <syedmirhabib@gmail.com>
2025-08-28 18:03:44 +0600

    build(config): update project configuration
    Modified files:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,23 @@
 import type { Config } from "tailwindcss"
 
 const config = {
+  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
+  prefix: "",
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "2rem",
-        screens: {
-          "2xl": "1400px",
-        },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
