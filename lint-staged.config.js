0ac00e7cce80609c7de05434dcab6b29dda593e9
ahsanhabibakik <syedmirhabib@gmail.com>
2025-08-31 21:54:46 +0600

    feat: auto commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,2 @@
-export default {
-  // Only prettify files for now - skip type checking and linting to make commits fast
-  "**/*.(ts|tsx|js|jsx|md|json)": filenames => `prettier --write ${filenames.join(" ")}`,
-};
+// Disabled for development speed
+export default {};
